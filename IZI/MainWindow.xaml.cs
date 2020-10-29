679eacf41e9ca37155c084f6f3c1fb96bd29cc59
blib0l <blib@inbox.ru>
2020-10-29 12:23:41 +0400

    izi

---
+++ IZI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IZI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string myHost = System.Net.Dns.GetHostName();//получаем имя ПК
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textBox.Text = myHost;
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            System.Net.IPAddress ip = System.Net.Dns.GetHostByName(myHost).AddressList[0];
+            textBox1.Text = Convert.ToString(ip);
+        }
+    }
+}
